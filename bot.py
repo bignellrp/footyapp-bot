e1aebcdc1d54c3481d9529b49cdc60da437fcf02
Richard Bignell <bignellrp@gmail.com>
2023-09-20 20:32:50 +0100

    Added command prefix to env

--- bot.py
+++ bot.py
@@ -1,33 +1,27 @@
 import discord
 import discord.ext.commands as commands
 from dotenv import load_dotenv
-from services.get_oscommand import IFBRANCH, GITBRANCH
 import os
 import logging
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('discord').setLevel(logging.ERROR)
+logging.getLogger('discord.http').setLevel(logging.WARNING)
 
 load_dotenv()  # Load environment variables from .env
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='$', intents=intents)
-TOKEN = os.getenv("DISCORD_TOKEN_DEV")
+command_prefix = os.getenv("COMMAND_PREFIX")
+bot = commands.Bot(command_prefix=command_prefix, intents=intents)
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 # for file in os.listdir("cogs"):
 #     if file.endswith(".py"):
 #         name = file[:-3]
 #         bot.load_extension(f"cogs.{name}")
 
-# @bot.event
-# async def on_ready():
-#     """Adds a message to the log when the bot logs in."""
-#     print(f"{bot.user.name} logged in successfully")
-
-if IFBRANCH in GITBRANCH:
-    CHANNEL_ID = os.getenv("CHANNEL_ID")
-else:
-    CHANNEL_ID = os.getenv("CHANNEL_ID_DEV")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 bot.load_extension("cogs.login")
 bot.load_extension("cogs.welcome")
